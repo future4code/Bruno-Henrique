eddc07e6b080601e344c3ce8211d2dbe82eabc38
Bruno <silva.bruno@outlook.com>
2020-12-07 14:26:30 -0300

    REsolução Aula13 - Exercicio4 a e b

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,35 @@
+//Exercicio1
+//a. Será impresso o valor 10 (2*5) e depois o valor 50 (10*5)
+//
+//b. Não seria impresso nada no console pois a função so retorna o calculo
+
+//Exercicio2
+//a. Após a chamar a função não nomeada passando o array com nomes como parametro, os nomes Darvas e Caio
+//serão impressos no console. Onde Darvas eh a primeira posição do array(index = 0 ) e Caio a segunda 
+//posição do array (index = 1);
+//
+//b. Serão impressos os nomes Amanda e em seguida Caio.
+
+//Exercico3
+//Essa função retorna um array numerico com valores (pares)^2. Talvez um nome "valorParAoQuadrado".
+
+//Exercicio4
+// function informacaoGenerica(){
+//     console.log("Meu nome é Bruno, tenho 37 anos, moro em Minas Gerais e já saltei de paraquedas.");
+// }
+// informacaoGenerica();
+
+// function informacaoGenerica2(nome, idade, estado, esportesRadicais){
+//     let afirmacao;
+//     if (esportesRadicais){
+//         afirmacao = "já";
+//     }else{
+//         afirmacao = "nunca";
+//     }
+//     let fraseGenerica = `Meu nome é ${nome}, tenho ${idade} anos, moro em ${estado} e ${afirmacao} saltei de paraquedas.`;
+    
+//     return fraseGenerica
+// }
+// console.log(informacaoGenerica2("Rafael", 35, "Curitiba", true));
+
+
