f0555b9db55bb8ec597d3a3527a7796a45cb58fc
Bruno <silva.bruno@outlook.com>
2020-12-08 21:41:23 -0300

    Logica para apagar todas as tasks

--- semana4/Planner/index.js
+++ semana4/Planner/index.js
@@ -1,34 +1,43 @@
 
-const selectionWeekDay = document.getElementById("dias-semana")
-
-//console.log(newTask)
+const selectionWeekDay = document.getElementById("dias-semana");
 //console.log(selectionWeekDay)
 
 function createTask() {
     const newTask = document.getElementById("tarefa")
-    const newTaskValue = newTask.value
-    const daySelected = selectionWeekDay.value
+    const newTaskValue = newTask.value;
+    const daySelected = selectionWeekDay.value;
     // console.log(newTaskValue);
     // console.log(daySelected);
 
     if (newTaskValue) {
-        const day = document.getElementById(daySelected)
-        day.innerHTML += `<li class="li-item"> ${newTaskValue} </li>`
-        //console.log(day);
+        const day = document.getElementById(daySelected);
+        // console.log(day);
+        day.innerHTML += `<li class="li-item"> ${newTaskValue} </li>`;
+        
     } else {
         alert("Informe uma tarefa");
     }
     newTask.value = '';
 }
 
+//Limpando o planner
+const weekDaysClean = document.querySelectorAll(".li-item");
+console.log(weekDaysClean)
+function clearTasks(){
+    if(confirm("Deseja apagar todas a tarefas:")){
+        for(let i = 0; i < weekDaysClean.length; i++){
+           weekDaysClean[i].innerHTML = ''; 
+        }
+    }
+}
+
 //Riscar tarefas
+const taskContainer = document.getElementById('weekdays-container');
+taskContainer.addEventListener('click', completeTask);
 
-const taskContainer = document.getElementById('task-container');
-taskContainer.addEventListener('click', riskItem)
-
-function riskItem(e){
+function completeTask(e) {
     //console.log(e.target)
-    if(e.target.classList.contains('li-item')){
-        e.target.classList.toggle('risked-item')
+    if (e.target.classList.contains('li-item')) {
+        e.target.classList.toggle('risked-item');
     }
 }
