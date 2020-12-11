8eebd5276cfe39ac11f5d83ddf3634d8e68fc3ac
Bruno <silva.bruno@outlook.com>
2020-12-11 11:19:44 -0300

    Interpretação - Exercicios 3 e 4

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -13,3 +13,18 @@ Serão impressos no console, o valor de 165 referente a variavel novoMontante e 
 segundoValor pois o tipoDeInvestimento foi informado errado, então a função exibe um alerta e como não há valor
 dentro da variavel de retorno, no console se´r exibido undefined. */
 
+//Exercicio 3
+/* Declarado os tres arrays(numeros,array1 e array2), o loop for/of e usado no array numeros onde numero irá
+receber os itens de cada posição do array e verificado se esse valor eh par ou impar e SE ele é par ele
+será colocado dentro do array1 SENAO é par ele será colodado no array2. Em ambos os casos é usado o a 
+o push para colocar o valor na ultima posição do array. A primeira mensagem no console será o tamanho do array
+numeros, a segunda mensagem será o tamanho do array1 e a terceira linha no console será o tamanho do array2
+pois foi usado o .length nos arrays*/
+
+//Exercico4
+/* Será impresso no console o menor e o maior numero do numeros respectivamente. Após declarado o array e as
+variaveis numero1 e numero2, o array entra no loop do for/of onde será compara o valor de cada item do array
+com as variaveis numero1 e numero2. SE o item na primeira posição do array eh menor que numero1, numero1 recebe
+o desse item, ou seja armazenando sempre o menor durante a interação. O inverso acontece com a variavel numero2
+onde somente os itens do array que forem maiores que a variavel numero2 serão colocados na propria variavel
+numero2. */
