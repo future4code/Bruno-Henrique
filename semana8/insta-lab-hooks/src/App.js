a6197e02f158dc8d4864c729302285c3c546b38a
Bruno <silva.bruno@outlook.com>
2021-01-25 20:40:33 -0300

    Criando um array de Posts

--- semana8/insta-lab-hooks/src/App.js
+++ semana8/insta-lab-hooks/src/App.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import Post from './components/Post/Post';
+import React, { useState } from 'react';
+// import Post from './components/Post/Post';
 import styled from "styled-components"
-// import SecaoNovaPostagem from './components/SecaoNovaPostagem/SecaoNovaPostagem';
+import SecaoNovaPostagem from './components/SecaoNovaPostagem/SecaoNovaPostagem';
 
-const AppContainer = styled.div `
+const AppContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
@@ -12,13 +12,54 @@ const AppContainer = styled.div `
 
 const App = () => {
 
+  const [postsList, setPostsList] = useState([])
+  const [post, setPost] = useState({
+    user: "",
+    avatar: "",
+    link: ""
+  })
+
+  const [inputUserName, setInputUserName] = useState('')
+  const [inputUserAvatar, setInputUserAvatar] = useState('')
+  const [inputLink, setInputLink] = useState('')
+
+  const handleInputUserName = (e) => {
+    setInputUserName(e.target.value)
+  }
+
+  const handleInputUserAvatar = (e) => {
+    setInputUserAvatar(e.target.value)
+  }
+
+  const handleInputLink = (e) => {
+    setInputLink(e.target.value)
+  }
+
+  const handlePostsList = () => {
+    setPost({
+      user: inputUserName,
+      avatar: inputUserAvatar,
+      link: inputLink
+    })
+    const newPostsList = [post, ...postsList]
+    setPostsList(newPostsList)
+    console.log(postsList)
+  }
+
   return (
     <AppContainer>
-      <Post
+      <SecaoNovaPostagem
+        inputUserName={inputUserName} onChangeName={handleInputUserName}
+        inputUserAvatar={inputUserAvatar} onChangeAvatar={handleInputUserAvatar}
+        inputLink={inputLink} onChangeLink={handleInputLink}
+        onClickCreate={handlePostsList}
+      />
+
+      {/* <Post
         nomeUsuario={'Usuário'}
         fotoUsuario={'https://picsum.photos/50/50'}
         fotoPost={'https://picsum.photos/200/150'}
-      />
+      /> */}
     </AppContainer>
   );
 }
--- semana8/insta-lab-hooks/src/components/SecaoNovaPostagem/SecaoNovaPostagem.js
+++ semana8/insta-lab-hooks/src/components/SecaoNovaPostagem/SecaoNovaPostagem.js
@@ -1,57 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const SecaoNovaPostagem = (props) => {
-    const [postsList, setPostsList] = useState([]);
-    const [post, setPost] = useState({})
-    const [inputUserName, setInputUserName] = useState('')
-    const [inputUserAvatar, setInputUserAvatar] = useState('')
-    const [inputLink, setInputLink] = useState('')
-
-
-    const handleInputUserName = (e) => {
-        setInputUserName(e.target.value)
-    }
-
-    const handleInputUserAvatar = (e) => {
-        setInputUserAvatar(e.target.value)
-    }
-
-    const handleInputLink = (e) => {
-        setInputLink(e.target.value)
-    }
-
-    const handleBtnPost = () => {
-        const newPostsList = [...postsList];
-
-        setPost({
-            Name: inputUserName,
-            Avatar: inputUserAvatar,
-            Picture: inputLink
-        })
-        console.log(post)
-    }    
 
     return (
         <div>
             <input
                 placeholder={"Nome de usuário"}
-                value={inputUserName}
-                onChange={handleInputUserName}
+                value={props.inputUser}
+                onChange={props.onChangeName}
 
             />
             <input
                 placeholder={"Imagem do avatar"}
-                value={inputUserAvatar}
-                onChange={handleInputUserAvatar}
+                value={props.inputAvatar}
+                onChange={props.onChangeAvatar}
 
             />
             <input
                 placeholder={"Link da imagem"}
-                value={inputLink}
-                onChange={handleInputLink}
+                value={props.inputLink}
+                onChange={props.onChangeLink}
 
             />
-            <button onClick={handleBtnPost}>Postar</button>
+            <button onClick={props.onClickCreate}>Postar</button>
         </div>
     )
 }
