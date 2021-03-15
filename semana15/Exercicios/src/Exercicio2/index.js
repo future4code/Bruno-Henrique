2274b8d1ac9d7ab5fab7a0f8ede4486efd92fd14
Bruno <silva.bruno@outlook.com>
2021-03-15 16:20:37 -0300

    Desafio Exercico 2

--- semana15/Exercicios/src/Exercicio2/index.js
+++ semana15/Exercicios/src/Exercicio2/index.js
@@ -1,21 +1,25 @@
-const num1 = Number(process.argv[3])
-const num2 = Number(process.argv[4])
+if (process.argv[2] && process.argv[3] && process.argv[4]) {
+    const num1 = Number(process.argv[3])
+    const num2 = Number(process.argv[4])
 
-switch (process.argv[2]) {
-    case "add":
-        console.log(`${num1} + ${num2} = ${num1 + num2}`)
-        break;
-    case "sub":
-        console.log(`${num1} - ${num2} = ${num1 - num2} `)
-        break;
-    case "mult":
-        console.log(`${num1} x ${num2} = ${num1 * num2}`)
-        break;
-    case "div":
-        console.log(`${num1} / ${num2} = ${num1 / num2}`)
-        break;
+    switch (process.argv[2]) {
+        case "add":
+            console.log(`${num1} + ${num2} = ${num1 + num2}`)
+            break;
+        case "sub":
+            console.log(`${num1} - ${num2} = ${num1 - num2} `)
+            break;
+        case "mult":
+            console.log(`${num1} x ${num2} = ${num1 * num2}`)
+            break;
+        case "div":
+            console.log(`${num1} / ${num2} = ${num1 / num2}`)
+            break;
 
-    default:
-        console.log("Operação não identificada!")
-        break;
+        default:
+            console.log(`${process.argv[2]} não uma operação válida!`)
+            break;
+    }
+}else{
+    console.log("Espero receber 3 parametros de entrada. ")
 }
