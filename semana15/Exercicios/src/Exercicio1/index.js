f2e02531cc80debbeff2be7fd8337f6c7cb146b6
Bruno <silva.bruno@outlook.com>
2021-03-15 16:07:35 -0300

    Desafios Exercico1

--- semana15/Exercicios/src/Exercicio1/index.js
+++ semana15/Exercicios/src/Exercicio1/index.js
@@ -5,10 +5,16 @@
 //b.)
 //Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima 
 //no console uma mensagem que siga a seguinte estrutura: "Olá, (Nome)! Você tem (sua idade) anos."
-const user = process.argv[2]
+const colors = require('colors')
+
+if(process.argv[2] && process.argv[3]){
+const user = process.argv[2].blue.bold;
 const age = Number(process.argv[3])
 console.log(`b.) Olá, ${user}! Você tem ${age} anos.`)
 
 //c.)
 //Altere o programa acima para que mostre também a sua idade daqui a sete anos.
-console.log(`c.) Olá, ${user}! Você tem ${age} anos.Em sete anos você terá ${age + 7} anos`)
+console.log(`c.) Olá, ${user}! Você tem ${age} anos.`+`Em sete anos você terá ${age + 7} anos`.black.bgCyan)
+}else{
+    console.log(colors.red("Parametros invalidos! Informe nome e idade."))
+}
