dd4f7c9451744adbc95d127b0591f0d25f01ab37
Bruno <silva.bruno@outlook.com>
2021-03-15 21:17:39 -0300

    Resolvendo um conflito

--- semana15/Exercicios/index.js
+++
@@ -1,9 +0,0 @@
-const listaDeTarefas = [
-    "Comprar pão",
-    "Lavar a moto",
-    "Preparar o jantar"
-]
-
-listaDeTarefas.push(process.argv[2])
-console.log("Tarefa adicionada com sucesso")
-console.log(`Tarefas: ${listaDeTarefas}`)
