b8be04eac7768f4d0dbbc7a9d16a1cd67e1dc690
Bruno <silva.bruno@outlook.com>
2021-03-17 12:06:31 -0300

    Endpoint1

--- semana15/Express/src/index.ts
+++ semana15/Express/src/index.ts
@@ -1,27 +1,30 @@
-// dentro do index.ts, fazer importações e ativar o Express e Cors.
-
-import express, {Express} from 'express'
+import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
+import { AddressInfo } from "net";
+import { countries } from './countries'
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-// a função express() inicia a aplicação web com express
-// os .use() ativam os módulos de Bodyparser e Cors
+app.get('/countries/all', (req: Request, res: Response) => {
+    const myCountries = countries;
+
+    const result = myCountries.map((ct) => ({
+        ctId: ct.id,
+        ctName: ct.name        
+    }));
+    res.status(201).send(result).end()
+})
 
 // esse código + essa importação para criar o servidor:
 // por performance, é bom o servidor ser o último trecho de código do documento
-
-
-import { AddressInfo } from "net";
-
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`);
     } else {
-       console.error(`Failure upon starting server.`);
+        console.error(`Failure upon starting server.`);
     }
 });
