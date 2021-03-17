35f547ede71dce694cc72301cb308e3af8a2ae99
Bruno <silva.bruno@outlook.com>
2021-03-17 12:15:04 -0300

    Endpoint2

--- semana15/Express/src/index.ts
+++ semana15/Express/src/index.ts
@@ -11,11 +11,19 @@ app.use(cors());
 app.get('/countries/all', (req: Request, res: Response) => {
     const myCountries = countries;
 
-    const result = myCountries.map((ct) => ({
+    const results = myCountries.map((ct) => ({
         ctId: ct.id,
-        ctName: ct.name        
+        ctName: ct.name
     }));
-    res.status(201).send(result).end()
+    res.status(201).send(results).end()
+})
+
+app.get('/countries/:id', (req: Request, res: Response) => {
+    const id: number = Number(req.params.id)
+    const results = countries.filter((ct) => {
+        return ct.id === id
+    })
+    res.status(201).send(results).end()
 })
 
 // esse código + essa importação para criar o servidor:
