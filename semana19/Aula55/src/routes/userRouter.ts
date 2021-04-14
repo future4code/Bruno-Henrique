2bd44737de185d60b6796b1b2d0f78a3b182d983
Bruno <silva.bruno@outlook.com>
2021-04-14 14:34:16 -0300

    Exercicio 1

---
+++ semana19/Aula55/src/routes/userRouter.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { login } from '../controller/login'
+import { signup } from '../controller/signup'
+
+export const userRouter = express()
+
+userRouter.post('/signup', signup)
+userRouter.post('/login', login)
