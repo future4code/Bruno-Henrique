c59b50cf2105a3dfff1d1a51db8305971cb6a467
Bruno <silva.bruno@outlook.com>
2021-05-28 18:09:06 -0300

    Exercicio 4 completo

--- logic-exercises/Exercise3/src/exercise4/exercise4.ts
+++ logic-exercises/Exercise3/src/exercise4/exercise4.ts
@@ -3,12 +3,16 @@ export const houseRobbery = (
     listOfHouses: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 ): number | string => {
 
-    const lastHouse: number = listOfHouses.length - 1  
+    const lastHouse: number = listOfHouses.length - 1
     let money: number = 0
     let house: number = 0
 
     money = thiefList.reduce((acc, value) => {
-
+        console.log(value);
+        
+        if (value > lastHouse) {
+            return 0
+        }
         if (acc === 0) {
             house = value - 1
             acc = listOfHouses[house]
@@ -26,5 +30,5 @@ export const houseRobbery = (
         return "Alarme foi acionado!"
     }
 
-    return money
+    return money > 0 ? money : "Lista de assalto errada"
 }
--- logic-exercises/Exercise3/src/index.ts
+++ logic-exercises/Exercise3/src/index.ts
@@ -1,5 +1,9 @@
 import { checaParenteses, checkParenteses } from "./exercise3/exercise3"
+import { houseRobbery } from "./exercise4/exercise4";
 
-const data = "{}"
+// const data = "{}"
 // console.log(checkParenteses(data))
-console.log(checaParenteses(data))
+// console.log(checaParenteses(data))
+
+const data = [10, 13, 15, 12]
+console.log(houseRobbery(data));
