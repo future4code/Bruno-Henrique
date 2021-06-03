5ac3991242816854c0def2ac146d8503211195d5
Bruno <silva.bruno@outlook.com>
2021-06-02 21:14:45 -0300

    Alterando o exercicio

--- logic-exercises/Exercise3/src/exercise6/exercise6.ts
+++ logic-exercises/Exercise3/src/exercise6/exercise6.ts
@@ -1,13 +1,15 @@
 export const funcSoma = (array: number[], target: number): number[] => {
+    let inicial: number[] = []
     let arrIndex = array.reduce((acc, value, idx, arr) => {
-        for (let index = idx + 1; index < array.length; index++) {
+        let index: number = idx + 1
+        for (index; index < array.length; index++) {
             if (value + arr[index] === target) {
                 acc.push(idx)
                 acc.push(index)
             }
         }
         return acc
-    }, [])
+    }, inicial)
 
     return arrIndex
 }
